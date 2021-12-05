b289ae0682b2bb13fd7b0348ba565bc4e81f87e7
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-05 20:25:44 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import s from  '../styles/global.module.css'
 import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
+import  'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Home() {
 return <div className ={s.body}>
