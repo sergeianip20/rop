ae434e223dde300117387afb8933d4c052a521b8
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-08 13:04:14 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import s from  '../styles/global.module.css'
-import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown, Container, Carousel } from 'react-bootstrap'
 import  'bootstrap/dist/css/bootstrap.min.css';
 import Link from 'next/link'
 
