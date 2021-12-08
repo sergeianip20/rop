5284a6ba91ff978742bbdae0ae67e05d98875798
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-08 12:26:54 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 export default function Home() {
 return <div className ={s.body}>
 
-
+<div>
 <Navbar collapseOnSelect expand="lg"  variant="dark">
    <Container>
 <Navbar.Brand href="#home">VPcake</Navbar.Brand>
@@ -25,8 +25,26 @@ return <div className ={s.body}>
 </Navbar.Collapse>
 </Container>
 </Navbar>
+</div>
+<div>
+<Carousel>
+<Carousel.Item>
+
+<img className = "d-block w-100"
+ src = "https://sun9-21.userapi.com/impg/ixAS_ZOSmAlxtmOUVPKevsc1vwSFtfAJpy6ihw/tg9OjjUfzlI.jpg?size=1280x854&quality=96&sign=ee67c5a5de61994abad691b68056b195&type=album"
+
+ alt = "hello"
+/>
+<Carousel.Caption>
+
+<h3 className ='slider_h3'>Торты от валентины</h3>
+
+</Carousel.Caption>
 
 
+</Carousel.Item>
+</Carousel>
+</div>
 </div>
 
 
