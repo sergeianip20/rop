67071ff3eda95c168ce8435687cfe18bb2ee26f3
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-08 14:32:33 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
 return <div>
 
 <div className ={s.body}>
-<Navbar collapseOnSelect expand="lg"  variant="dark">
+<Navbar collapseOnSelect expand="lg" >
    <Container>
 <Navbar.Brand href="#home">VPcake</Navbar.Brand>
 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
