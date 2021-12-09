26839062644fce4a244cf3b806bea3d1cc99aebe
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-09 14:03:55 +0400

    Update price.js

--- pages/price.js
+++ pages/price.js
@@ -1,7 +1,53 @@
+import s from  '../styles/global.module.css'
+import {Navbar, Nav, NavDropdown, Container, Carousel, Card, CardGroup } from 'react-bootstrap'
+import  'bootstrap/dist/css/bootstrap.min.css';
+import Link from 'next/link'
+import Image from 'next/image'
+import Chr from '../public/Chr.jpg'
+import Profile from '../public/Profile.jpg'
+import Vk from '../public/vk.png'
+import Inst from '../public/inst.jpg'
+
+
 
 export default function price() {
 return <div>
-Hello
+<div className ={s.body}>
+<Navbar collapseOnSelect expand="lg"  variant="dark">
+   <Container>
+<Navbar.Brand href="#home">VPcake</Navbar.Brand>
+<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+ <Navbar.Collapse id="responsive-navbar-nav">
+<Nav className="me-auto">
+<NavDropdown title="Торты" id="collasible-nav-dropdown">
+<NavDropdown.Item > <Link href= '/secon'><a className = {s.sa}>детские</a></Link> </NavDropdown.Item>
+<NavDropdown.Item > <Link href= '/'><a className = {s.sa}>заказ</a></Link> </NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item > <Link href= '/price'><a className = {s.sa}>именные</a></Link> </NavDropdown.Item>
+</NavDropdown>
+<Nav.Link href="#features">Прайс</Nav.Link>
+</Nav>
+</Navbar.Collapse>
+</Container>
+</Navbar>
+</div>
+<div>
+<Carousel>
+<Carousel.Item>
+<Image src={Chr} alt="Vercel Logo" layout="intrinsic"  />
+<Carousel.Caption>
+<h3>Торты</h3>
+</Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+<Image src={Chr} alt="Vercel Logo"  layout="intrinsic" />
+<Carousel.Caption>
+<h3>Торты</h3>
+</Carousel.Caption>
+</Carousel.Item>
+</Carousel>
+</div>
+</div>
 </div>
 
 }
