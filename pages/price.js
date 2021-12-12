25b1e0fda9a0821db7d2d088a481c4f1834817e0
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-12 12:35:00 +0400

    Update price.js

--- pages/price.js
+++ pages/price.js
@@ -1,5 +1,5 @@
 import s from  '../styles/global.module.css'
-import {Navbar, Nav, NavDropdown, Container, Carousel, Card, CardGroup } from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown, Container, Carousel, Card, CardGroup, Modal  } from 'react-bootstrap'
 import  'bootstrap/dist/css/bootstrap.min.css';
 import Link from 'next/link'
 import Image from 'next/image'
