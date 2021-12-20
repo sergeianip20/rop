c49cb7aa87d7ca2c6b365541fe5d1f74807dc7f1
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-20 20:51:49 +0400

    Update anniversary.js

--- pages/anniversary.js
+++ pages/anniversary.js
@@ -8,7 +8,29 @@ import Image from 'next/image'
 
 export default  function anniversary(){
 return<div>
-Hello
+
+<div className ={s.body}>
+<Navbar collapseOnSelect expand="lg"  variant="dark">
+   <Container>
+<Navbar.Brand href="#home">VPcake</Navbar.Brand>
+<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+ <Navbar.Collapse id="responsive-navbar-nav">
+<Nav className="me-auto">
+<NavDropdown title="Торты" id="collasible-nav-dropdown">
+<NavDropdown.Item > <Link href= '/secon'><a className = {s.sa}>детские</a></Link> </NavDropdown.Item>
+<NavDropdown.Item > <Link href= '/'><a className = {s.sa}>заказ</a></Link> </NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item > <Link href= '/price'><a className = {s.sa}>Свадебные</a></Link> </NavDropdown.Item>
+
+<NavDropdown.Item > <Link href= '/anniversary'><a className = {s.sa}>Юбилейные</a></Link> </NavDropdown.Item>
+
+</NavDropdown>
+<Nav.Link href="#features">Прайс</Nav.Link>
+</Nav>
+</Navbar.Collapse>
+</Container>
+</Navbar>
+</div>
 </div>
 
 }
