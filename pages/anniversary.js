ad6ee4354efc2578f86c3b92c66a90ffefd59cb7
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-20 20:48:56 +0400

    Update anniversary.js

--- pages/anniversary.js
+++ pages/anniversary.js
@@ -1,3 +1,11 @@
+import s from  '../styles/global.module.css'
+import {Navbar, Nav, NavDropdown, Container, Carousel, Card, CardGroup } from 'react-bootstrap'
+import  'bootstrap/dist/css/bootstrap.min.css';
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+
 export default  function anniversary(){
 return<div>
 Hello
